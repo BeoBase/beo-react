d782dd341e3f196e171560776bb897c94ccdc460
Bellamy Phan <55559722+bellamyphan@users.noreply.github.com>
2026-08-07 16:46:19 -0500

    BEO-46: set testing coverage to 70 perent (#15)
    * BEO-46: update gitignore file to exclude testing coverage output
    
    * BEO-46: install vite testing coverage
    
    * BEO-46: update GitHub action ci pr title script
    
    * BEO-46: update GitHub action ci npm build script to include testing
    
    * BEO-46: add the first unit tests for App.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-      react(),
-      tailwindcss(),
+    react(),
+    tailwindcss()
   ],
-})
+  
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: "v8",
+      reporter: [
+        "text",
+        "html"
+      ],
+      thresholds: {
+        lines: 70,
+        functions: 70,
+        branches: 70,
+        statements: 70
+      }
+    }
+  }
+});
