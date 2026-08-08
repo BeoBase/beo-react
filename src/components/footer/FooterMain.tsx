f0027685401fd761783cf93385855d1b51329f95
Bellamy Phan <55559722+bellamyphan@users.noreply.github.com>
2026-08-07 19:25:58 -0500

    BEO-208: delete css and use tailwind css only (#20)

--- src/components/footer/FooterMain.tsx
+++ src/components/footer/FooterMain.tsx
@@ -1,8 +1,8 @@
 
 export default function FooterMain() {
   return (
-    <footer className="w-full border-t border-gray-200 py-5 text-center mt-auto">
-      <p className="text-sm text-gray-600">
+    <footer className="w-full border-t border-gray-200 py-5 text-center mt-auto backdrop-blur-xs">
+      <p className="text-sm font-medium text-gray-200 drop-shadow-md">
         © {new Date().getFullYear()} BeoBase.com. All rights reserved.
         <br />
         Author: Bellamy Phan
