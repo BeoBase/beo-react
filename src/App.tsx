ff86cc3a11e774e61ed411a127ce51583d5aeebe
Bellamy Phan <bellamyphan@icloud.com>
2026-08-09 18:37:44 -0500

    BEO-77: draft Dev and Portfolio pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import NavbarMain from "./components/navbar/NavbarMain.tsx";
 import Home from "./pages/Home.tsx";
 import SignUp from "./pages/SignUp.tsx";
 import SignIn from "./pages/SignIn.tsx";
+import Development from "./pages/Development.tsx";
+import Portfolio from "./pages/Portfolio.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 function App() {
@@ -21,6 +23,8 @@ function App() {
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Home/>}/>
+        <Route path="/dev" element={<Development />}/>
+        <Route path="/portfolio" element={<Portfolio />}/>
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         
